f93b8a0529685614478c3ef50327c7b83aef74af
美权 戴 <daimeiquan@hotmail.com>
2016-03-20 13:10:33 +0800

    1

---
+++ CrystalQuartz.Example.RemoteScheduler.Server/PrintMessageJob.cs
@@ -0,0 +1,15 @@
+﻿namespace CrystalQuartz.Example.RemoteScheduler.Server
+{
+    using System;
+    using Quartz;
+
+    public class PrintMessageJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            for(int i = 0; i < 1000; i++) { 
+            Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
